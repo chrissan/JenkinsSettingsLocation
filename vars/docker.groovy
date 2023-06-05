cba5a3267c86803b5e98e10838022d12b333bf1b
Christian  Sanchez <christiansanchez@MacBook-Pro-de-Christian.local>
2023-06-05 17:52:45 -0600

    Se particiona en dos el flujo del pipeline

---
+++ vars/docker.groovy
@@ -0,0 +1,37 @@
+def call(){
+    pipeline{
+    agent: any
+    environment {
+        pom = readMavenPom file: 'pom.xml'
+        tag = sh(returnStdout: true, script: "echo $pom | awk -F':' '{print \$2 \":\" $env.BUILD_NUMBER}'")
+    }
+    stages{
+        stage('Download Dockerfile')
+            steps{
+                git branch: 'main', credentialsId: 'chris', url: 'git@github.com:BanCoppelUnity/pipeline-config.git'
+                sh 'cp pipeline-config/dockerfiles/archetypes/springboot/Dockerfile .'
+            }
+
+        stage('Dockerize') {
+            steps {
+                sh 'printenv'
+                sh 'docker build -t $tag .'
+            }
+        }
+
+        stage('Push Docker image') {
+            environment {
+                password = credentials('harborCredentials')
+            }
+            steps{
+                sh 'docker login -u admin -p $password $harborURL'
+                sh 'docker tag $tag tempservices.eastus.cloudapp.azure.com/archetype/$tag'
+                sh 'docker push tempservices.eastus.cloudapp.azure.com/archetype/$tag'
+                sh 'docker rmi $tag'
+                sh 'docker rmi tempservices.eastus.cloudapp.azure.com/archetype/$tag'
+            }
+        }
+        }
+    }
+}
+
--- vars/springboot.groovy
+++ vars/springboot.groovy
@@ -33,33 +33,6 @@ def call(){
                     !OSADependencies.json, !**/node_modules/**/*, !**/.cxsca-results.json, !**/.cxsca-sast-results.json, !.checkmarx/cx.config''', fullScanCycle: 10, groupId: '88', password: '{AQAAABAAAAAQbBNhu0/vp69ntf2YCBmiLUQIRA2dNr4q13KlSvWUnoM=}', preset: '0', projectName: 'TestUnityCM', sastEnabled: true, serverUrl: 'https://coppel.checkmarx.net', sourceEncoding: '1', username: '', vulnerabilityThresholdResult: 'FAILURE', waitForResultsEnabled: true])
             }
         }
-
-
-
-        stage('Dockerize') {
-            environment {
-                pom = readMavenPom file: 'pom.xml'
-                tag = sh(returnStdout: true, script: "echo $pom | awk -F':' '{print \$2 \":\" $env.BUILD_NUMBER}'")
-            }
-            steps {
-                sh 'printenv'
-                sh 'docker build -t $tag .'
-            }
-        }
-
-        stage('Push Docker image') {
-            environment {
-                password = credentials('harborCredentials')
-                pom = readMavenPom file: 'pom.xml'
-                tag = sh(returnStdout: true, script: "echo $pom | awk -F':' '{print \$2 \":\" $env.BUILD_NUMBER}'")
-            }
-            steps{
-                sh 'docker login -u admin -p $password $harborURL'
-                sh 'docker tag $tag tempservices.eastus.cloudapp.azure.com/archetype/$tag'
-                sh 'docker push tempservices.eastus.cloudapp.azure.com/archetype/$tag'
-                sh 'docker system prune -f -a'
-            }
-        }
         }
     }
 }
